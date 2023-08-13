d5a95b4a87ae08eb3f9cce191025b13d1e37c321
unknown <binyaalex@gmail.com>
2023-08-13 15:36:02 +0300

    email and name when its not from links

--- index.js
+++ index.js
@@ -18,6 +18,43 @@ const port = process.env.PORT || 3000;
 // Set up storage for multer
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
+function extractNamesFromText(text) {
+  const lines = text.split('\n');
+  let words = [];
+
+  // Split lines into words
+  for (const line of lines) {
+    words = words.concat(line.trim().split(' '));
+  }
+
+  // Identify consecutive capitalized words as potential names
+  let potentialNames = [];
+  let currentName = '';
+
+  for (const word of words) {
+    if (word.match(/^[A-Z][a-z]*$/)) {
+      currentName += word + ' ';
+    } else if (word.match(/^[A-Z][a-z]*[A-Z][a-z]*$/)) {
+      // Handle names without spaces
+      potentialNames.push(word);
+    } else {
+      if (currentName.trim() !== '') {
+        potentialNames.push(currentName.trim());
+        currentName = '';
+      }
+    }
+  }
+
+  // Convert names without spaces to names with spaces
+  const namesWithSpaces = potentialNames.map(name => {
+    if (name.match(/^[A-Z][a-z]*[A-Z][a-z]*$/)) {
+      return name.replace(/([a-z])([A-Z])/g, '$1 $2');
+    }
+    return name;
+  });
+
+  return namesWithSpaces;
+}
 
 connectDB()
 app.use("/", express.static("public"));
@@ -50,51 +87,83 @@ app.post('/upload', upload.single('pdf'), async (req, res) => {
     }
     const pdfBuffer = req.file.buffer;
     
-    // linkdin name and email
-    pdfExtract.extractBuffer(pdfBuffer, options, (err, data) => {
-        if (err) return console.log(err);
-        data.pages[0].links.forEach(element => {
-            // console.log(element);
-            if (element.includes("linkedin")) {
-                console.log(element);
-                applicantObj.linkedin = element
-                applicantObj.name = element.slice(element.indexOf("in/")+3, element.lastIndexOf("-"))
-            }
-            
-            if (element.includes("mailto") && element.includes("@")) {
-                console.log(element);
-                applicantObj.email = element.slice(element.indexOf(":")+1, element.length)
-            }
+    // links - linkdin name and email
+    try {
+      const data = await new Promise((resolve, reject) => {
+        pdfExtract.extractBuffer(pdfBuffer, options, (err, extractedData) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(extractedData);
+          }
         });
-    });
+      });
+  
+      console.log(data.pages[0].links);
+  
+      for (const element of data.pages[0].links) {
+        if (element.includes("linkedin")) {
+          applicantObj.linkedin = element;
+          const nameStartIndex = element.indexOf("in/") + 3;
+          const nameEndIndex = element.lastIndexOf("-");
+          applicantObj.name = element.slice(nameStartIndex, nameEndIndex);
+        }
+  
+        if (element.includes("mailto") && element.includes("@")) {
+          applicantObj.email = element.slice(element.indexOf(":") + 1);
+        }
+      }
+  
+      // Rest of your code
+    } catch (error) {
+      console.error('Error extracting PDF:', error);
+      // Handle the error
+    }
+    
 
 
     // mobile and id
     const pdfDataParsed = await pdfParse(pdfBuffer);
+    // console.log(pdfDataParsed);
     const extractedText = pdfDataParsed.text;
     const mobileRe = /(?:[-+() ]*\d){10,13}/gm; 
-    const idRe = /(?:[ ]*\d){9,9}/gm; 
     applicantObj.mobile = extractedText.match(mobileRe)?.map(function(s){return s.trim();})[0] || null;
+    const idRe = /\b\d{9}\b/gm; 
     applicantObj.id = extractedText.match(idRe)?.map(function(s){return s.trim();})[0] || null;
+
+    // mail name and linkdin if it didn't work from the links
+
+    //mail
+    if (!applicantObj.email) {
+      const emailRe = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/;
+      applicantObj.email = emailRe.exec(extractedText).map(function(s){return s.trim();})[0] || null;
+    }
+
+    //name
+    if (!applicantObj.name) {
+      const extractedNames = extractNamesFromText(pdfDataParsed.text);
+      console.log(extractedNames);
+      applicantObj.name = extractedNames[0]
+    }
 
     // save to MongoDB
     console.log(applicantObj);
-    const applicant = new Applicant({
-        name: applicantObj.name,
-        email: applicantObj.email,
-        id: applicantObj.id,
-        linkedin: applicantObj.linkedin,
-        mobile: applicantObj.mobile,
-        rawData: pdfBuffer,
-    });
+    // const applicant = new Applicant({
+    //     name: applicantObj.name,
+    //     email: applicantObj.email,
+    //     id: applicantObj.id,
+    //     linkedin: applicantObj.linkedin,
+    //     mobile: applicantObj.mobile,
+    //     rawData: pdfBuffer,
+    // });
 
-    try {
-    await applicant.save();
-        res.status(200).json({ message: 'Data saved successfully' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error saving data' });
-    }
+    // try {
+    // await applicant.save();
+    //     res.status(200).json({ message: 'Data saved successfully' });
+    // } catch (error) {
+    //     console.error(error);
+    //     res.status(500).json({ message: 'Error saving data' });
+    // }
 });
 
 // Route for download file
